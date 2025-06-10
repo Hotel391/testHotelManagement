93d51e081ead73bfd0500181401efc4aea86b26d
githubhieukaisa <vuahoarong@gmail.com>
2025-06-10 23:12:13 +0700

    solution

--- src/java/models/Constants.java
+++ src/java/models/Constants.java
@@ -9,15 +9,15 @@ package models;
  * @author Hai Long
  */
 public class Constants {
-        public static String GOOGLE_CLIENT_ID = "";
+        public static final String GOOGLE_CLIENT_ID = "";
 
-	public static String GOOGLE_CLIENT_SECRET = "";
+	public static final String GOOGLE_CLIENT_SECRET = "";
 
-	public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/fptHotel/login";
+	public static final String GOOGLE_REDIRECT_URI = "http://localhost:9999/fptHotel/login";
 
-	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+	public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 
-	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+	public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
 
-	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+	public static final String GOOGLE_GRANT_TYPE = "authorization_code";
 }
